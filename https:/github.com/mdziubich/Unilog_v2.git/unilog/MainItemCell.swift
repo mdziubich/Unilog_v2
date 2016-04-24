5f6117e253db8c85df7ef97f7c4c2e8d8564751b
Małgorzata Dziubich <malgorzata.dziubich@mbank.pl>
2016-04-24 23:22:34 +0200

    first initial commit

---
+++ https:/github.com/mdziubich/Unilog_v2.git/unilog/MainItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainItemCell.swift
+//  unilog
+//
+//  Created by Małgorzata Dziubich on 24/04/16.
+//  Copyright © 2016 Małgorzata Dziubich. All rights reserved.
+//
+
+import UIKit
+
+class MainItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ https:/github.com/mdziubich/Unilog_v2.git/unilog/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  unilog
+//
+//  Created by Małgorzata Dziubich on 24/04/16.
+//  Copyright © 2016 Małgorzata Dziubich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+//extension ViewController: UITableViewDataSource {
+//    
+//}
